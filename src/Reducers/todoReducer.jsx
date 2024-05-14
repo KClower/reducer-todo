ab98f4c402068a97fb9be53ba167e37036430b92
KClower <kclower3@gmail.com>
2024-05-14 16:25:14 -0500

    finished project

---
+++ src/Reducers/todoReducer.jsx
@@ -0,0 +1,47 @@
+
+export const initialState = [
+    {
+        item: 'Clean Kitchen',
+        completed: false,
+        id: 3892987589
+    },
+
+    {
+        item: 'Clean Bedroom',
+        completed: false,
+        id: new Date(),
+
+    }
+]
+
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_TASK":
+            return [
+                ...state, { item: action.payload, completed: false, id: new Date() }
+            ]
+        case "TOGGLE_COMPLETED":
+            // find todo by id
+            // highlight found todo from state
+            // update state
+            return state.map(todo => {
+                if (todo.id === action.payload) {
+                    return { ...todo, completed: !todo.completed }
+                }
+                else {
+                    return todo
+                }
+            })
+        case "CLEAR_COMPLETED":
+            return state.filter(todo => {
+                if (todo.completed === true) {
+                    return ""
+                }
+                else return todo
+            })
+
+        default: return state;
+
+    }
+}
